3fd15068aec2408919202f2f17ee92b1dcd9c67b
chiragvadodariya <chirag@jeavio.com>
2025-01-14 10:40:14 +0530

    test(add): Created test for custom delimiter and neative numbers.

--- test_calculator.py
+++ test_calculator.py
@@ -10,5 +10,15 @@ class TestCalculator(unittest.TestCase):
         calc = Calculator();
         self.assertEqual(calc.add("1\n2,3"), 6);
         
+    def testCustomDelimiter(self):
+        calc = Calculator();
+        self.assertEqual(calc.add("//;\n1;2"), 3);
+        
+    def testNegativeNumbers(self):
+        calc = Calculator();
+        with self.assertRaises(ValueError) as exc_info:
+            calc.add("-1,2,-3");
+        self.assertEqual(str(exc_info.exception), "Negatives not allowed: -1, -3");
+        
 if __name__ == '__main__':
     unittest.main();
